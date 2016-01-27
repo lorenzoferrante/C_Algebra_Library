541dca14110b7d0aa82c21a292b9b300039897b9
Lorenzo Ferrante <lorenzoferrante@me.com>
2016-01-27 18:31:30 +0100

    Create Algebra.h

---
+++ Algebra.h
@@ -0,0 +1,31 @@
+//
+// Created by Lorenzo Ferrante on 27/01/16.
+//
+
+#ifndef ALGEBRA_ALGEBRA_H
+#define ALGEBRA_ALGEBRA_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*============================
+ * New type matrix.
+ *==========================*/
+typedef struct _matrix {
+    size_t M, N;
+    double *data;
+} matrix;
+
+/*============================
+ * Display error clearly
+ *==========================*/
+extern void assert(int assertion, char *message);
+
+/*============================
+ * Useful algebra functions
+ *==========================*/
+extern matrix *createMatrix(size_t row, size_t col);
+extern matrix *createFullMatrix(size_t row, size_t col, double *data);
+extern matrix *copyMatrix(matrix *m);
+
+#endif //ALGEBRA_ALGEBRA_H
